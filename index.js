280d5847cf4d97f3c9b1d6c16b2dd68ccd7c1b3a
Ds777774 <sjemma819@gmail.com>
2025-01-14 13:06:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -68,8 +68,8 @@ const sendWordOfTheDay = async () => {
   }
 };
 
-// Schedule Word of the Day at 13:04 IST daily (adjusted for UTC)
-cron.schedule('34 7 * * *', sendWordOfTheDay, {
+// Schedule Word of the Day at 13:10 IST daily (adjusted for UTC)
+cron.schedule('40 7 * * *', sendWordOfTheDay, {
   scheduled: true,
   timezone: "Asia/Kolkata"
 });
