2d5f6b82da0d4637077e54d4b94037c0a70936da
Ds777774 <sjemma819@gmail.com>
2025-02-03 15:17:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,29 @@
 const { Client, GatewayIntentBits } = require('discord.js');
-const { Pool } = require('pg');
 const dotenv = require('dotenv');
-const duelCommand = require('./duel');  // Import the duel command
-
 dotenv.config();
 
-// Set up PostgreSQL connection pool
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-});
+// Load commands
+const duel = require('./duel');
 
-// Create a new Discord client
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMessageReactions,
         GatewayIntentBits.GuildMembers,
     ],
 });
 
-// Log in to Discord using the bot token from environment variables
-client.login(process.env.BOT_TOKEN);
-
-// When the bot is ready
 client.once('ready', () => {
-    console.log(`${client.user.tag} is now online!`);
+    console.log(`${client.user.tag} has logged in!`);
 });
 
-// Command handler
-client.on('messageCreate', async (message) => {
-    // Ignore messages from the bot itself
-    if (message.author.bot) return;
-
-    // Check if the message is a duel command
+// Handling the !duel command
+client.on('messageCreate', (message) => {
     if (message.content.startsWith('!duel')) {
-        // Split the message to get the list of players
-        const args = message.content.split(' ').slice(1);
-        if (args.length < 2) {
-            return message.channel.send('Please mention at least two players for the duel.');
-        }
-
-        // Call the duel command handler
-        await duelCommand.execute(message, args);
+        duel.handleDuel(message);
     }
 });
+
+client.login(process.env.DISCORD_TOKEN);
